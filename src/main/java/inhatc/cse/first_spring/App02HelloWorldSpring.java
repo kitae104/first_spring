6cc7f112d12faff7a6c1a73a0f7664de47ffdfa6
Kitae's Github <aqua0405@gmail.com>
2024-01-26 17:13:25 +0900

    beans 처리

--- src/main/java/inhatc/cse/first_spring/App02HelloWorldSpring.java
+++ src/main/java/inhatc/cse/first_spring/App02HelloWorldSpring.java
@@ -13,6 +13,11 @@ public class App02HelloWorldSpring
 		// 2: 스프링이 관리하기 원하는 항목을 구성 - @Configuration 클래스를 이용한 구성
 		var name = context.getBean("name", String.class);
 		System.out.println("name: " + name);
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("person2MethodCall"));
+		System.out.println(context.getBean("person3Parameters"));
+		//System.out.println(context.getBean(Address.class));
 	}
 
 }
--- src/main/java/inhatc/cse/first_spring/HelloWorldConfiguration.java
+++ src/main/java/inhatc/cse/first_spring/HelloWorldConfiguration.java
@@ -3,11 +3,45 @@ package inhatc.cse.first_spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {}
+
+record Address(String dong, String gu) {}
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name(){
         return "Kim Ki Tae";
+    }
+    
+    @Bean
+    public int age(){
+        return 15;
+    }
+    
+    @Bean
+    public Person person() {
+    	var person = new Person("Kim Ki Tae", 20, new Address("사당동", "동작구"));
+    	return person;
+    }
+    
+    @Bean
+    public Person person2MethodCall() {
+    	return new Person(name(), age(), address2());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {  // name, age, address
+    	return new Person(name, age, address3);
+    }
+
+    @Bean(name="address2")
+    public Address address2() {
+    	return new Address("구월동 롯데캐슬 ", "남동구");
+    }
+    @Bean(name="address3")
+    public Address address3() {
+    	return new Address("학익동 ", "미추홀구");
     }
 }
